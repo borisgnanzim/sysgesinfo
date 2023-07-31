3d0243b30e5b91f0c8099b0c6ca4341719d0b0d3
borisgnanzim <borisgnanzim@gmail.com>
2023-07-31 10:18:54 +0200

    commit 31-07-2023 18h18

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index', function () {
+    return view('NiceAdmin/index');
+})->name('dashbord');
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,28 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                    // ajout  28-07-2023 11h42
+                'resources/assets/img/favicon.png',
+                'resources/assets/img/apple-touch-icon.png',
+                'resources/assets/js/main.js',
+                'resources/assets/vendor/bootstrap/css/bootstrap.min.css',
+                //
+                'resources/assets/vendor/bootstrap-icons/bootstrap-icons.css',
+                'resources/assets/vendor/boxicons/css/boxicons.min.css',
+                'resources/assets/vendor/quill/quill.snow.css',
+                'resources/assets/vendor/quill/quill.bubble.css',
+                'resources/assets/vendor/remixicon/remixicon.css',
+                'resources/assets/css/style.css',
+                'resources/assets/vendor/simple-datatables/style.css',
+                //
+                'resources/assets/vendor/apexcharts/apexcharts.min.js',
+                'resources/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/vendor/chart.js/chart.umd.js',
+                'resources/assets/vendor/echarts/echarts.min.js',
+                'resources/assets/vendor/quill/quill.min.js',
+                'resources/assets/vendor/simple-datatables/simple-datatables.js',
+                'resources/assets/vendor/tinymce/tinymce.min.js',
+                'resources/assets/vendor/php-email-form/validate.js',
             ],
             refresh: true,
         }),
